c7ff890f667c6bd68d95499ba6860ec818cc3e29
Hiu <88535737+RebecatEL@users.noreply.github.com>
2023-11-03 21:24:22 -0400

    LabEx 3

---
+++ 101395302_COMP3123-exec03/Employee.js
@@ -0,0 +1,28 @@
+//TODO - Create Employee Module here and export to use in index.js
+class Employees {
+    constructor(id, firstName, lastName, email, salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.Salary = salary;
+    }
+
+    display() {
+        return Employees.toString();
+    }
+}
+
+const testData = [
+    { id: 1, firstName: "Pritesh", lastName: "Patel", email: "pritesh@gmail.com", Salary: 5000 },
+    { id: 2, firstName: "Krish", lastName: "Lee", email: "krish@gmail.com", Salary: 4000 },
+    { id: 3, firstName: "Racks", lastName: "Jacson", email: "racks@gmail.com", Salary: 5500 },
+    { id: 4, firstName: "Denial", lastName: "Roast", email: "denial@gmail.com", Salary: 9000 }
+]
+
+const employees = testData.map(data => new Employees(data.id, data.firstName, data.lastName, data.email, data.Salary));
+
+module.exports = {
+    Employees,
+    employees,
+};
